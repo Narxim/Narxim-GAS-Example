5f3a3a227e72b2b2846a055482983706a694372b
Joey Thigpen <45883189+Narxim@users.noreply.github.com>
2021-02-09 12:50:58 -0500

    Added additional comments

--- Source/GAS_Example/Private/Characters/NonPlayerCharacter.cpp
+++ Source/GAS_Example/Private/Characters/NonPlayerCharacter.cpp
@@ -17,17 +17,20 @@ ANonPlayerCharacter::ANonPlayerCharacter()
 
 	// Sets the Ability System Component to use Minimal replication mode. This will only replicate minimal Gameplay Effect info to connected clients.
 	NPC_AbilitySystemComponent->ReplicationMode = EGameplayEffectReplicationMode::Minimal;
-
+	
+	// Set the TWeakObjectPtr from Character Base to the newly created Ability System Component sub-object.
 	AbilitySystemComponent = NPC_AbilitySystemComponent;
 	
 	// Create the Health Attribute Set sub-object.
 	NPC_HealthAttributes = CreateDefaultSubobject<UHealthAttributeSet>(FName("Health Attributes"));
 
+	// Set the TWeakObjectPtr from Character Base to the newly created Health Attribute Set sub-object.
 	HealthAttributes = NPC_HealthAttributes;
 	
 	// Create the Stamina Attribute Set sub-object.
 	NPC_StaminaAttributes = CreateDefaultSubobject<UStaminaAttributeSet>(FName("Stamina Attributes"));
 
+	// Set the TWeakObjectPtr from Character Base to the newly created Stamina Attribute Set sub-object.
 	StaminaAttributes = NPC_StaminaAttributes;
 }
 

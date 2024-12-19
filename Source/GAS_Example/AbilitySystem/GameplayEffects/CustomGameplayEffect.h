f5da29ac7cd335695215585740680d308ff781c4
franckblais <franck.blais@gmail.com>
2024-12-18 21:21:16 -0500

    Adding CustomGameplayEffect class
    Empty for now.

---
+++ Source/GAS_Example/AbilitySystem/GameplayEffects/CustomGameplayEffect.h
@@ -0,0 +1,16 @@
+﻿// Copyright 2021 Joseph "Narxim" Thigpen.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "CustomGameplayEffect.generated.h"
+
+/**
+ * Custom gameplay effect for the project.
+ */
+UCLASS()
+class GAS_EXAMPLE_API UCustomGameplayEffect : public UGameplayEffect
+{
+	GENERATED_BODY()
+};

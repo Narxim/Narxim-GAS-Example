87242a9199b5f4341eae601b0fbc1ac7d0131485
Steveeey <6006425-Steveeey@users.noreply.gitlab.com>
2025-02-14 22:44:27 +0000

    Create turn system interface which specifies how the turn should be managed.

---
+++ Source/GAS_Example/Game/TurnSystemInterface.h
@@ -0,0 +1,33 @@
+// Copyright 2021 Joseph "Narxim" Thigpen.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "TurnSystemInterface.generated.h"
+
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnTurnChange, int32 /*NewTurn*/);
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UTurnSystemInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * Manage the current turn and broadcast the `OnTurnChanged` event.
+ */
+class GAS_EXAMPLE_API ITurnSystemInterface
+{
+	GENERATED_BODY()
+
+public:
+	virtual FOnTurnChange& GetOnTurnChangeDelegate() = 0;
+	
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Turn System")
+	int32 GetCurrentTurn() const;
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Turn System")
+	void IncrementTurn();
+};

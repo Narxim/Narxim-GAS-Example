8a50c71b6f925d3ee88fb4b80fb1e5c5706aab11
Steveeey <6006425-Steveeey@users.noreply.gitlab.com>
2025-02-14 22:51:01 +0000

    Create custom GameplayEffect that simplifies the creation of Turn Based Gameplay Effects for designers.

---
+++ Source/GAS_Example/AbilitySystem/GameplayEffects/TurnBasedGameplayEffect.cpp
@@ -0,0 +1,83 @@
+// Copyright 2021 Joseph "Narxim" Thigpen.
+
+
+#include "TurnBasedGameplayEffect.h"
+
+#include "GAS_Example/AbilitySystem/AbilitySystemComponent/TurnBasedGameplayEffectComponent.h"
+#include "Misc/DataValidation.h"
+
+#define LOCTEXT_NAMESPACE "TurnBasedGameplayEffect"
+
+UTurnBasedGameplayEffect::UTurnBasedGameplayEffect()
+{
+	DurationPolicy = EGameplayEffectDurationType::Infinite;
+}
+
+void UTurnBasedGameplayEffect::PostInitProperties()
+{
+	Super::PostInitProperties();
+    
+	// Force infinite duration regardless of designer input
+	DurationPolicy = EGameplayEffectDurationType::Infinite;
+    
+	// Clear duration magnitude values
+	DurationMagnitude = FGameplayEffectModifierMagnitude();
+	
+	FindOrAddComponent<UTurnBasedGameplayEffectComponent>();
+}
+
+#if WITH_EDITOR
+void UTurnBasedGameplayEffect::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+    
+	// Force infinite duration if changed
+	if(PropertyChangedEvent.GetPropertyName() == GET_MEMBER_NAME_CHECKED(UGameplayEffect, DurationPolicy))
+	{
+		DurationPolicy = EGameplayEffectDurationType::Infinite;
+	}
+}
+
+EDataValidationResult UTurnBasedGameplayEffect::IsDataValid(class FDataValidationContext& Context) const
+{
+	EDataValidationResult Result = Super::IsDataValid(Context);
+
+	// Validate duration policy
+	if (DurationPolicy != EGameplayEffectDurationType::Infinite)
+	{
+		Context.AddError(FText::FormatOrdered(LOCTEXT("InvalidDurationPolicyError",
+			"[{0}] Requires Infinite Duration - Parent GE must use 'Infinite' duration policy to work with turn-based components"),
+			FText::FromString(GetClass()->GetName())));
+		Result = EDataValidationResult::Invalid;
+	}
+
+	// Check for required component
+	if (FindComponent<UTurnBasedGameplayEffectComponent>() == nullptr)
+	{
+		Context.AddError(FText::FormatOrdered(LOCTEXT("MissingComponentError",
+			"[{0}] Missing Required Component - Add a TurnBasedGameplayEffectComponent to enable turn-based behavior"),
+			FText::FromString(GetClass()->GetName())));
+		Result = EDataValidationResult::Invalid;
+	}
+
+	// Validate modifiers
+	for (auto Modifier : Modifiers)
+	{
+		if (!Modifier.Attribute.IsValid())
+		{
+			Context.AddError(FText::FormatOrdered(LOCTEXT("InvalidModifierAttributeError",
+				"[{0}] Invalid Modifier Configuration\n"
+				"Modifiers must either:\n"
+				"• Set a valid Attribute\n"
+				"• Be removed entirely if unused\n"
+				"Empty attributes prevent GE application, including component effects"),
+				FText::FromString(GetClass()->GetName())));
+			Result = EDataValidationResult::Invalid;
+		}
+	}
+	
+	return Result;
+}
+#endif
+
+#undef LOCTEXT_NAMESPACE
